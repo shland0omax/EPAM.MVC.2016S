134216065634872859d0e9da4cb6defb5a6215ca
Максим Михайлович Шландаков <shland0omax@gmail.com>
2016-08-31 11:33:45 +0300

    Created

---
+++ Day1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(int? id)
+        {
+            if (id == null) id = 0;
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
